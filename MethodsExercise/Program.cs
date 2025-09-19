f7671833a88c0da5e1deb730ec0b2d18998ff851
Alexis Boegner <alexisboegner@gmail.com>
2025-09-18 23:36:21 -0400

    Created branch ArithmeticUserInput

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -25,82 +25,81 @@ namespace MethodsExercise
             Console.WriteLine($"The quotient of integers twenty-one & six is {QuotientOfIntegers(21, 6)}");
         }
         
-        //Console.WriteLine("Please, enter numbers to sum then type 'done' to finish!");
-           //while (true)
-          //  {
-            //    string input = Console.ReadLine();
-             //   if (input.ToLower() == "done")
-             //   {
-             //       break;
-             //   }
+        Console.WriteLine("Please, enter numbers to sum then type 'done' to finish!");
+           while (true)
+           {
+               string input = Console.ReadLine();
+               if (input.ToLower() == "done")
+               {
+                   break;
+               }
+               if (int.TryParse(input, out int number))
+               {
+                   SumOfIntegers(number);
+               }
+               else
+               {
+                   Console.WriteLine("Invalid input. Please enter a number or 'done'");
+               }
+           }
+           Console.WriteLine("The difference of integers!");
+           Console.WriteLine("Please, enter the numbers to subtract then type 'done' to finish!");
+           while (true)
+           {
+               string input = Console.ReadLine();
+               if (input.ToLower() == "done")
+               {
+                   break;
+               }
 
-             //   if (int.TryParse(input, out int number))
-             //   {
-                //    SumOfIntegers(number);
-             //   }
-             //   else
-              //  {
-              //      Console.WriteLine("Invalid input. Please enter a number or 'done'");
-           //     }
-          //  }
+               if (int.TryParse(input, out int number))
+               {
+                   DifferenceOfIntegers(number);
+               }
+               else
+               {
+                   Console.WriteLine("Invalid input. Please enter a number or 'done'");
+               }
+           }
+           Console.WriteLine("The product of integers!");
+           Console.WriteLine("Please, enter the numbers to multiply then type 'done' to finish!");
+           while (true)
+           {
+               string input = Console.ReadLine();
+               if (input.ToLower() == "done")
+               {
+                   break;
+               }
 
-           // Console.WriteLine("The difference of integers!");
-            //Console.WriteLine("Please, enter the numbers to subtract then type 'done' to finish!");
-           // while (true)
-           // {
-            //    string input = Console.ReadLine();
-            //    if (input.ToLower() == "done")
-             //   {
-              //      break;
-              //  }
-              //  if (int.TryParse(input, out int number))
-               // {
-                 //   DifferenceOfIntegers(number);
-               // }
-               // else
-               // {
-                //    Console.WriteLine("Invalid input. Please enter a number or 'done'");
-               // }
-           // }
+               if (int.TryParse(input, out int number))
+               {
+                   ProductOfIntegers(number);
+               }
+               else
+               {
+                   Console.WriteLine("Invalid input. Please enter a number or 'done'");
+               }
+           }
+           Console.WriteLine("The quotient of integers!");
+           Console.WriteLine("Please, enter the numbers to multiply then type 'done' to finish!");
+           while (true)
+           {
+               string input = Console.ReadLine();
+               if (input.ToLower() == "done")
+               {
+                   break;
+               }
 
-           // Console.WriteLine("The product of integers!");
-           // Console.WriteLine("Please, enter the numbers to multiply then type 'done' to finish!");
-            //while (true)
-            //{
-             //   string input = Console.ReadLine();
-             //   if (input.ToLower() == "done")
-             //   {
-              //      break;
-              //  }
-              //  if (int.TryParse(input, out int number))
-             //   {
-              //      ProductOfIntegers(number);
-               // }
-               // else
-               // {
-                //    Console.WriteLine("Invalid input. Please enter a number or 'done'");
-                //}
-           // }
-
-           // Console.WriteLine("The quotient of integers!");
-           // Console.WriteLine("Please, enter the numbers to multiply then type 'done' to finish!");
-           // while (true)
-           // {
-            //    string input = Console.ReadLine();
-            //    if (input.ToLower() == "done")
-            //    {
-            //        break;
-             //   }
-              //  if (int.TryParse(input, out int number))
-              //  {
-              //      QuotientOfIntegers(number);
-             //   }
-             //   else
-             //   {
-             //       Console.WriteLine("Invalid input. Please enter a number or 'done'");
-             //   }
-           // }
-
+               if (int.TryParse(input, out int number))
+               {
+                   QuotientOfIntegers(number);
+               }
+               else
+               {
+                   Console.WriteLine("Invalid input. Please enter a number or 'done'");
+               }
+           }
+           
            public static void DayAtTheZoo(string userName, string favoriteZooAnimal, string animalFood, string fedAnimal,
                 string zooSouvenir)
             {
@@ -111,18 +110,18 @@ namespace MethodsExercise
                 Console.WriteLine($"{userName} purchased a {zooSouvenir} at the zoo gift shop!");
             }
 
-           public static int SumOfIntegers(params int[] numbers)
+           private static int SumOfIntegers(params int[] numbers)
            {
               int sumInteger = 0;
                foreach (int number in numbers)
               {
                     sumInteger += number;
                }
-            //    Console.WriteLine($"The sum is {sumInteger}");
-                return sumInteger;
+               Console.WriteLine($"The sum is {sumInteger}");
+               return sumInteger;
             }
 
-            public static int DifferenceOfIntegers(params int[] numbers)
+            private static int DifferenceOfIntegers(params int[] numbers)
             {
                 if (numbers.Length == 0) return 0;
                 int differenceInteger = numbers[0];
@@ -130,29 +129,29 @@ namespace MethodsExercise
                 {
                     differenceInteger -= number;
                 }
-              //  Console.WriteLine($"The difference is {differenceInteger}.");
+                Console.WriteLine($"The difference is {differenceInteger}.");
                 return differenceInteger;
             }
 
-            public static int ProductOfIntegers(params int[] numbers)
+            private static int ProductOfIntegers(params int[] numbers)
             {
                 int productInteger = 1;
                 foreach (int number in numbers)
                 {
                     productInteger *= number;
                 }
-             //   Console.WriteLine($"The product is {productInteger}");
+                Console.WriteLine($"The product is {productInteger}");
                 return productInteger;
             }
 
-            public static double QuotientOfIntegers(params double[] numbers)
+            private static double QuotientOfIntegers(params double[] numbers)
             {
                 double quotientInteger = numbers[0];
                 foreach (double number in numbers)
                 {
                     quotientInteger /= number;
                 }
-            //    Console.WriteLine($"The quotient is {quotientInteger}");
+                Console.WriteLine($"The quotient is {quotientInteger}");
                 return quotientInteger;
             }
     }
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879bc51ef403450a0a853307a5e2bad9e738048e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959bc8a59a00fb7b01866910998559a43c3147a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
